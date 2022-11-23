c1c000e2e4c7565afcd95f955bc57586ac11e227
Kevin Longboy <kevinlongboy@gmail.com>
2022-11-23 07:44:02 -0800

    User seeders.

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -81,7 +81,7 @@ module.exports = (sequelize, DataTypes) => {
         allowNull: false,
         type: DataTypes.STRING,
         validate: {
-          len: [4, 30],
+          len: [3, 30],
           isNotEmail(value) {
             if (Validator.isEmail(value)) {
               throw new Error("Cannot be an email.");
--- backend/db/seeders/20221121024830-demo-user.js
+++ backend/db/seeders/20221121024830-demo-user.js
@@ -1,31 +1,454 @@
 'use strict';
 const bcrypt = require("bcryptjs");
 
+// {
+//   firstName: '',
+//   lastName: '',
+//   username: '',
+//   hashedPassword: bcrypt.hashSync(''),
+//   email: '@appacademy.io',
+// },
+
+const demoUsers = [
+  // Module Leads
+  {
+    firstName: 'Bill',
+    lastName: 'Adams',
+    username: 'wadams',
+    hashedPassword: bcrypt.hashSync('wadams'),
+    email: 'wadams@appacademy.io',
+  },
+  {
+    firstName: 'Shane',
+    lastName: 'Wilkey',
+    username: 'swilkey',
+    hashedPassword: bcrypt.hashSync('swilkey'),
+    email: 'swilkey@appacademy.io',
+  },
+  {
+    firstName: 'Geoffrey',
+    lastName: 'Otieno',
+    username: 'gotieno',
+    hashedPassword: bcrypt.hashSync('gotieno'),
+    email: 'gotieno@appacademy.io',
+  },
+  {
+    firstName: 'Dan',
+    lastName: 'Purcell',
+    username: 'dpurcell',
+    hashedPassword: bcrypt.hashSync('dpurcell'),
+    email: 'dpurcell@appacademy.io',
+  },
+  {
+    firstName: 'Brad',
+    lastName: 'Simpson',
+    username: 'bsimpson',
+    hashedPassword: bcrypt.hashSync('bsimpson'),
+    email: 'bsimpson@appacademy.io',
+  },
+  {
+    firstName: 'Hector',
+    lastName: 'Crespo',
+    username: 'hcrespo',
+    hashedPassword: bcrypt.hashSync('hcrespo'),
+    email: 'hcrespo@appacademy.io',
+  },
+  // TA's
+  {
+    firstName: 'Cody',
+    lastName: 'Lavene',
+    username: 'clavene',
+    hashedPassword: bcrypt.hashSync('clavene'),
+    email: 'clavene@appacademy.io',
+  },
+  {
+    firstName: 'Cameron',
+    lastName: 'Whiteside',
+    username: 'cwhiteside',
+    hashedPassword: bcrypt.hashSync('cwhiteside'),
+    email: 'cwhiteside@appacademy.io',
+  },
+  {
+    firstName: 'Peter',
+    lastName: 'Shin',
+    username: 'pshin',
+    hashedPassword: bcrypt.hashSync('pshin'),
+    email: 'pshin@appacademy.io',
+  },
+  {
+    firstName: 'Braxton',
+    lastName: 'Kappes',
+    username: 'bkappes',
+    hashedPassword: bcrypt.hashSync('bkappes'),
+    email: 'bkappes@appacademy.io',
+  },
+  {
+    firstName: 'Jesse',
+    lastName: 'Christensen',
+    username: 'jchristensen',
+    hashedPassword: bcrypt.hashSync('jchristensen'),
+    email: 'jchristensen@appacademy.io',
+  },
+  {
+    firstName: 'Jojo',
+    lastName: 'Yih',
+    username: 'jyih',
+    hashedPassword: bcrypt.hashSync('jyih'),
+    email: 'jyih@appacademy.io',
+  },
+  {
+    firstName: 'Nick',
+    lastName: 'Esqueda',
+    username: 'nesqueda',
+    hashedPassword: bcrypt.hashSync('nesqueda'),
+    email: 'nesqueda@appacademy.io',
+  },
+  {
+    firstName: 'Andrew',
+    lastName: 'Fava',
+    username: 'afava',
+    hashedPassword: bcrypt.hashSync('afava'),
+    email: 'afava@appacademy.io',
+  },
+  {
+    firstName: 'Ari',
+    lastName: 'Robinson',
+    username: 'arobinson',
+    hashedPassword: bcrypt.hashSync('arobinson'),
+    email: 'arobinson@appacademy.io',
+  },
+  {
+    firstName: 'Sam',
+    lastName: 'Song',
+    username: 'ssong',
+    hashedPassword: bcrypt.hashSync('ssong'),
+    email: 'ssong@appacademy.io',
+  },
+  {
+    firstName: 'Tanner',
+    lastName: 'Shaw',
+    username: 'tshaw',
+    hashedPassword: bcrypt.hashSync('tshaw'),
+    email: 'tshaw@appacademy.io',
+  },
+  {
+    firstName: 'Olivia',
+    lastName: 'Bir',
+    username: 'obir',
+    hashedPassword: bcrypt.hashSync('obir'),
+    email: 'obir@appacademy.io',
+  },
+  {
+    firstName: 'Nate',
+    lastName: 'Treadaway',
+    username: 'ntreadway',
+    hashedPassword: bcrypt.hashSync('ntreadway'),
+    email: 'ntreadway@appacademy.io',
+  },
+  {
+    firstName: 'Daniel',
+    lastName: 'Chin',
+    username: 'dchin',
+    hashedPassword: bcrypt.hashSync('dchin'),
+    email: 'dchin@appacademy.io',
+  },
+  {
+    firstName: 'Alex',
+    lastName: 'Pezzati',
+    username: 'apezzati',
+    hashedPassword: bcrypt.hashSync('apezzati'),
+    email: 'apezzati@appacademy.io',
+  },
+  {
+    firstName: 'Sherry',
+    lastName: 'Yu',
+    username: 'syu',
+    hashedPassword: bcrypt.hashSync('syu'),
+    email: 'syu@appacademy.io',
+  },
+  {
+    firstName: 'Rawaha',
+    lastName: 'Memon',
+    username: 'rmemon',
+    hashedPassword: bcrypt.hashSync('rmemon'),
+    email: 'rmemon@appacademy.io',
+  },
+  {
+    firstName: 'Whit',
+    lastName: 'Minson',
+    username: 'wminson',
+    hashedPassword: bcrypt.hashSync('wminson'),
+    email: 'wminson@appacademy.io',
+  },
+  {
+    firstName: 'Daniel',
+    lastName: 'Flores',
+    username: 'dflores',
+    hashedPassword: bcrypt.hashSync('dflores'),
+    email: 'dflores@appacademy.io',
+  },
+  {
+    firstName: 'David',
+    lastName: 'Nash',
+    username: 'dnash',
+    hashedPassword: bcrypt.hashSync('dnash'),
+    email: 'dnash@appacademy.io',
+  },
+  {
+    firstName: 'John',
+    lastName: 'Lee',
+    username: 'jlee',
+    hashedPassword: bcrypt.hashSync('jlee'),
+    email: 'jlee@appacademy.io',
+  },
+  {
+    firstName: 'Andrew',
+    lastName: 'Tran',
+    username: 'atran',
+    hashedPassword: bcrypt.hashSync('atran'),
+    email: 'atran@appacademy.io',
+  },
+  {
+    firstName: 'Cesar',
+    lastName: 'Solano',
+    username: 'csolano',
+    hashedPassword: bcrypt.hashSync('csolano'),
+    email: 'csolano@appacademy.io',
+  },
+  {
+    firstName: 'Ryan',
+    lastName: 'Schneider',
+    username: 'rschneider',
+    hashedPassword: bcrypt.hashSync('rschneider'),
+    email: 'rschneider@appacademy.io',
+  },
+  {
+    firstName: 'Anthony',
+    lastName: 'Lovern',
+    username: 'alovern',
+    hashedPassword: bcrypt.hashSync('alovern'),
+    email: 'alovern@appacademy.io',
+  },
+  {
+    firstName: 'Franco',
+    lastName: 'Portin',
+    username: 'fportin',
+    hashedPassword: bcrypt.hashSync('fportin'),
+    email: 'fportin@appacademy.io',
+  },
+  {
+    firstName: 'Kevin',
+    lastName: 'Bartolome',
+    username: 'kbartolome',
+    hashedPassword: bcrypt.hashSync('kbartolome'),
+    email: 'kbartolome@appacademy.io',
+  },
+
+  // Cohort Leads
+  {
+    firstName: 'David',
+    lastName: 'DeHerrera',
+    username: 'ddeherrera',
+    hashedPassword: bcrypt.hashSync('ddeherrera'),
+    email: 'ddeherrera@appacademy.io',
+  },
+  {
+    firstName: 'Drew',
+    lastName: 'Thurman',
+    username: 'athruman',
+    hashedPassword: bcrypt.hashSync('athruman'),
+    email: 'athruman@appacademy.io',
+  },
+  {
+    firstName: 'David',
+    lastName: 'Rogers',
+    username: 'drogers',
+    hashedPassword: bcrypt.hashSync('drogers'),
+    email: 'drogers@appacademy.io',
+  },
+
+  // Students
+  {
+    firstName: 'Jair',
+    lastName: 'Carbajal',
+    username: 'jcarbajal',
+    hashedPassword: bcrypt.hashSync('jcarbajal'),
+    email: 'jcarbajal@appacademy.io',
+  },
+  {
+    firstName: 'Tiffany',
+    lastName: 'Yang',
+    username: 'tyang',
+    hashedPassword: bcrypt.hashSync('tyang'),
+    email: 'tyang@appacademy.io',
+  },
+  {
+    firstName: 'Daniel',
+    lastName: 'Lam',
+    username: 'lain',
+    hashedPassword: bcrypt.hashSync('lain'),
+    email: 'lain@appacademy.io',
+  },
+  {
+    firstName: 'Gary',
+    lastName: 'Song',
+    username: 'hustling',
+    hashedPassword: bcrypt.hashSync('hustling'),
+    email: 'hustling@appacademy.io',
+  },
+  {
+    firstName: 'Jake',
+    lastName: 'Matillano',
+    username: 'doofey',
+    hashedPassword: bcrypt.hashSync('doofey'),
+    email: 'doofey@appacademy.io',
+  },
+  {
+    firstName: 'John',
+    lastName: 'Carrera',
+    username: 'badreg',
+    hashedPassword: bcrypt.hashSync('badreg'),
+    email: 'badreg@appacademy.io',
+  },
+  {
+    firstName: 'Maliha',
+    lastName: 'Omair',
+    username: 'maliha',
+    hashedPassword: bcrypt.hashSync('maliha'),
+    email: 'maliha@appacademy.io',
+  },
+  {
+    firstName: 'Thenu',
+    lastName: 'Nachiyappan',
+    username: 'thenu',
+    hashedPassword: bcrypt.hashSync('thenu'),
+    email: 'thenu@appacademy.io',
+  },
+  {
+    firstName: 'Andrew',
+    lastName: 'Kay',
+    username: 'rewd',
+    hashedPassword: bcrypt.hashSync('rewd'),
+    email: 'rewd@appacademy.io',
+  },
+  {
+    firstName: 'Adam',
+    lastName: 'Selki',
+    username: 'Damzzz123',
+    hashedPassword: bcrypt.hashSync('Damzzz123'),
+    email: 'Damzzz123@appacademy.io',
+  },
+  {
+    firstName: 'Christopher',
+    lastName: 'Pannella',
+    username: 'goatedIndeed',
+    hashedPassword: bcrypt.hashSync('goatedIndeed'),
+    email: 'goatedIndeed@appacademy.io',
+  },
+  {
+    firstName: 'Christo',
+    lastName: 'Grabowski',
+    username: 'ChristoGrabowski',
+    hashedPassword: bcrypt.hashSync('ChristoGrabowski'),
+    email: 'ChristoGrabowski@appacademy.io',
+  },
+  {
+    firstName: 'Cindy',
+    lastName: 'Guzman',
+    username: 'Cece',
+    hashedPassword: bcrypt.hashSync('CeceRose'),
+    email: 'CeceRose@appacademy.io',
+  },
+  {
+    firstName: 'David',
+    lastName: 'Burch',
+    username: 'DavidDanielBurch',
+    hashedPassword: bcrypt.hashSync('DavidDanielBurch'),
+    email: 'DavidDanielBurch@appacademy.io',
+  },
+  {
+    firstName: 'Eliza',
+    lastName: 'Wimberly',
+    username: 'ElizaW',
+    hashedPassword: bcrypt.hashSync('ElizaW'),
+    email: 'ElizaW@appacademy.io',
+  },
+  {
+    firstName: 'James',
+    lastName: 'Lee',
+    username: 'JamesLee',
+    hashedPassword: bcrypt.hashSync('JamesLee'),
+    email: 'JamesLee@appacademy.io',
+  },
+  {
+    firstName: 'Jason',
+    lastName: 'Kong',
+    username: 'JasonKong',
+    hashedPassword: bcrypt.hashSync('JasonKong'),
+    email: 'JasonKong@appacademy.io',
+  },
+  {
+    firstName: 'Joanna',
+    lastName: 'Gilbert',
+    username: 'joanna',
+    hashedPassword: bcrypt.hashSync('joanna'),
+    email: 'joanna@appacademy.io',
+  },
+  {
+    firstName: 'Max',
+    lastName: 'Yaswen',
+    username: 'MaxYaswen',
+    hashedPassword: bcrypt.hashSync('MaxYaswen'),
+    email: 'MaxYaswen@appacademy.io',
+  },
+  {
+    firstName: 'Moran',
+    lastName: 'Even',
+    username: 'MoranEven',
+    hashedPassword: bcrypt.hashSync('MoranEven'),
+    email: 'MoranEven@appacademy.io',
+  },
+  {
+    firstName: 'Yasha',
+    lastName: 'Yang',
+    username: 'yashaY',
+    hashedPassword: bcrypt.hashSync('yashaY'),
+    email: 'yashaY@appacademy.io',
+  },
+  {
+    firstName: 'Yibo',
+    lastName: 'Guo',
+    username: 'YiboGuo',
+    hashedPassword: bcrypt.hashSync('YiboGuo'),
+    email: 'YiboGuo@appacademy.io',
+  },
+  {
+    firstName: 'Alex',
+    lastName: 'Auch',
+    username: 'Auchie',
+    hashedPassword: bcrypt.hashSync('Auchie'),
+    email: 'Auchie@appacademy.io',
+  },
+]
+
+let usernames = [];
+demoUsers.forEach(user => usernames.push(user.username))
+
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('Users', [
-      {
-        email: 'demo@email.com',
-        username: 'demo_user',
-        hashedPassword: bcrypt.hashSync('demoPassword')
-      },
-      {
-        email: 'user1@user.io',
-        username: 'FakeUser1',
-        hashedPassword: bcrypt.hashSync('password2')
-      },
-      {
-        email: 'user2@user.io',
-        username: 'FakeUser2',
-        hashedPassword: bcrypt.hashSync('password3')
-      }
-    ], {});
+    return queryInterface.bulkInsert(
+      'Users',
+      demoUsers,
+      {}
+    )
   },
 
   down: async (queryInterface, Sequelize) => {
     const Op = Sequelize.Op;
-    return queryInterface.bulkDelete('Users', {
-      username: { [Op.in]: ['Demo-lition', 'FakeUser1', 'FakeUser2'] }
-    }, {});
+    return queryInterface.bulkDelete(
+      'Users',
+      { username: { [Op.in]: usernames } },
+      {}
+    )
   }
 };
