a42eeb381ed3609e0b924f93c61cf666d521dc68
Kevin Longboy <kevinlongboy@gmail.com>
2022-11-27 01:23:18 -0800

    Departments backend route

--- backend/routes/api/departments.js
+++ backend/routes/api/departments.js
@@ -1,21 +1,30 @@
+/******************************** IMPORTS ********************************/
+// libraries
 const express = require('express');
-const { Op } = require('sequelize');
+const { Op, EmptyResultError } = require('sequelize');
 const { check } = require('express-validator');
-
+// local files
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { handleValidationErrors } = require('../../utils/validation');
-const { Department } = require('../../db/models');
+const { Department, Product, ProductImage } = require('../../db/models');
 
+
+/*************************** GLOBAL VARIABLES ****************************/
 const router = express.Router();
 
 
+/******************************** ROUTES *********************************/
+// Get all routes
 router.get('/', async(req, res) => {
 
     let error = {};
 
     try {
         let getAllDepartments = await Department.findAll({
-            raw: true
+            attributes: {
+                exclude: ["createdAt", "updatedAt"]
+            },
+            raw: true,
         })
 
         return res
@@ -25,20 +34,66 @@ router.get('/', async(req, res) => {
             })
 
     } catch (err) {
-        error.error = err
+        error.error = err;
         return res.json(error);
     };
 });
 
+// Get all products by department
+router.get('/:departmentId/products', async(req, res) => {
+
+    let currDeptId = req.params.departmentId;
+    let error = {};
+
+    try {
+        let findDept = await Department.findByPk(currDeptId)
+
+        if (!findDept) {
+            error.message = "Department couldn't be found";
+            error.statusCode = 404;
+            return res.status(404).json(error);
+
+        } else {
+            let findAllDepartmentProducts = await Product.findAll({
+                where: { departmentId: currDeptId},
+                attributes: {
+                    exclude: ["createdAt", "updatedAt"]
+                },
+                raw: true,
+            })
+
+            // add previewImage key to every product
+            for (let i = 0; i < findAllDepartmentProducts.length; i++) {
+                let product = findAllDepartmentProducts[i];
+                let prevImage = await ProductImage.findOne({
+                    where: { productId: product.id },
+                    raw: true
+                });
+                let prevUrl = prevImage.url;
+                product.previewImage = prevUrl
+            }
+
+            return res
+                .status(200)
+                .json({
+                    "id": findDept.id,
+                    "name": findDept.name,
+                    "Products": findAllDepartmentProducts
+                })
+        }
+
+    } catch (err) {
+        error.error = err;
+        return res.json(error);
+    };
+})
 
 
-/*************************************** error handler ****************************************/
-
+/***************************** ERROR HANDLER *****************************/
 router.use((err, req, res, next) => {
     return res.json(err)
 })
 
 
-/****************************************** export ********************************************/
-
+/******************************** EXPORTS ********************************/
 module.exports = router;
