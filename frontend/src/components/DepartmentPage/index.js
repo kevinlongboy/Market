0580387ba2eb7cd0a73763285a3952522c71216d
Kevin Longboy <kevinlongboy@gmail.com>
2022-11-29 02:30:07 -0800

    DepartmentPage styling

--- frontend/src/components/DepartmentPage/index.js
+++ frontend/src/components/DepartmentPage/index.js
@@ -29,26 +29,81 @@ function DepartmentPage() {
   /**************** render component *****************/
   return (
     <div className="page-wrapper-container">
-
       <div id="DepartmentPage-component">
 
-          <h1>{department.name}</h1>
-          {productsArr.map((product) => (
-            <>
-              <Link
-                key={`${product.id}`}
-                to={`/departments/${departmentId}/products/${product.id}`}
-              >
-              <h2>{product.name}</h2>
-
-              </Link>
-
-              <p>{(product.price).toFixed(2)}</p>
-              <p>{product.description}</p>
-            </>
-          ))}
+      <div className='DepartmentPage-title'>
+        <div>{department.name}</div>
       </div>
 
+      <div className="DepartmentPage-product-quantity-results">
+        <p>{productsArr.length} results</p>
+      </div>
+
+      <div className="product-card-container">
+        {productsArr.map((product) => (
+          <div className="product-card">
+
+            <div className="product-card-thumbnail-container">
+            <Link
+              exact
+              to={`/departments/${departmentId}/products/${product.id}`}
+            >
+              <img
+              src={product.previewImage}
+              className="product-card-thumbnail"
+              ></img>
+            </Link>
+            </div>
+
+          <div
+          className="product-card-name-container"
+          >
+            <Link
+              key={`${product.id}`}
+              exact
+              to={`/departments/${departmentId}/products/${product.id}`}
+              className="product-card-name"
+              // className="product-card-name-container"
+              >
+              <p
+              className="product-card-name"
+              >{product.name}</p>
+            </Link>
+          </div>
+
+          <div className="product-card-brand-container">
+            <p className="product-card-brand">Brand</p>
+          </div>
+
+          <div className="product-card-price-container">
+            <div className="product-card-price">${(product.price).toFixed(2)}</div>
+            <div className="product-card-location">Online</div>
+          </div>
+
+
+          <div className="product-card-CartAdd-container">
+
+            <div className="product-card-CartAdd-Shipt-offer">
+              <span>Get it as soon as 9am today</span> with Shipt
+            </div>
+
+            <div className="product-card-CartAdd-ISPU-offer">
+              <span>Ready within 2 hours</span> with pickup
+            </div>
+
+            <button className="product-card-CartAdd-button">
+              <p>Deliver it</p>
+            </button>
+
+          </div>
+
+
+          </div>
+
+        ))}
+      </div>
+
+      </div>
     </div>
   )
 }
